bfb7a7ff33b921ed215215f1cd57d2be931feab4
chunhua chen <chunhua.kpg@gmail.com>
2022-11-17 17:02:29 +0000

    create model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    acct = models.CharField(max_length=64, primary_key=True)
+    pwd = models.CharField(max_length=128)
+    fullname = models.CharField(max_length=64)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
