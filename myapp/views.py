be42d9533f0c684706af21d70035f098693c8b10
chunhua chen <chunhua.kpg@gmail.com>
2022-11-19 12:32:26 +0000

    hash the password

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ import json
 import time
 import jwt
 import datetime
+import bcrypt
 import sqlalchemy, sqlalchemy.orm
 import logging
 from django.shortcuts import render
@@ -42,7 +43,6 @@ def gen_token(user):
     return encoded_jwt
 
 def verify_token(request):
-    return True, ''
     account = None
     ret = False
     decode_success = False
@@ -72,67 +72,38 @@ def verify_token(request):
     
 
 def user_login(request):
-    Session = sqlalchemy.orm.sessionmaker(bind=engine)
-    session = Session()
+    if not check_request_method(request, 'POST'):
+        return JsonResponse({"result": FAILURE, "message": "wrong request method"}, status=405)
 
-    result = ""
-    msg = ""
-    resp = {}
-    d = json.loads(request.body)
-    if "user" in d and "pwd" in d:
-        acct = d["user"]
-        pwd = d["pwd"]
-        user = session.query(Users).filter_by(acct=acct,pwd=pwd).first()
-        if user:
-            result = "success"
-            msg = "login success"
-            resp["token"] = gen_token(user)
-    else:
-        result = "failure"
-        msg = "no account or no passwod information"
-    resp["result"] = result
-    resp["message"] = msg
-    return JsonResponse(resp)
+    try:
+        d = json.loads(request.body)
+    except ValueError as e:
+        logger.error("body:%r not a json data", request.body)
+        return JsonResponse({"result": FAILURE, "message": "wrong request body"}, status=400)
 
+    if "account" in d and "pwd" in d:
+        Session = sqlalchemy.orm.sessionmaker(bind=engine)
+        session = Session()
 
-def create_user(request):
-    Session = sqlalchemy.orm.sessionmaker(bind=engine)
-    session = Session()
-
-    d = json.loads(request.body)
-    user_acct = d["user"]
-    acct = session.query(Users).filter(Users.acct==user_acct).first()
-    result = ""
-    msg = ""
-    if not acct:
-        user_pwd = d["pwd"]
-        if "fullname" in d:
-            user_fullname = d["fullname"]
+        acct = d["account"]
+        passwd = d["pwd"]
+        user = session.query(Users).filter_by(acct=acct).first()
+        if user and bcrypt.checkpw(passwd.encode('utf-8'), user.pwd.encode('utf-8')):
+            return JsonResponse({"result": SUCCESS, "message": "login success.", "token": gen_token(user)})
         else:
-            user_fullname = user_acct
-
-        usr = Users()
-        usr.acct = user_acct
-        usr.pwd = user_pwd
-        usr.fullname = user_fullname
-        dt_tz = datetime.datetime.now()
-        dt = dt_tz.replace(tzinfo=None)
-        usr.created_at = dt
-        usr.updated_at = dt
-        session.add(usr)
-        session.commit()
-        result = "success"
-        msg = "user created."
+            return JsonResponse({"result": FAILURE, "message": "unauthorized"}, status=401)
     else:
-        result = "failure"
-        msg = "this account existed."
-    resp = {"result": result, "message": msg}
-    return JsonResponse(resp)
+        return JsonResponse({"result": FAILURE, "message": "wrong request body"}, status=400)
+
 
 def list_user(request):
+    if not check_request_method(request, 'GET'):
+        return JsonResponse({"result": FAILURE, "message": "wrong request method"}, status=405)
+
     token_verified, account_in_token = verify_token(request)
     if not token_verified:
-        raise exceptions.AuthenticationFailed("token verified failed.")
+        return JsonResponse({"result": FAILURE, "message": "token verification failure"}, status=403)
+
     Session = sqlalchemy.orm.sessionmaker(bind=engine)
     session = Session()
 
@@ -141,47 +112,48 @@ def list_user(request):
     for usr in users:
         usr = {"account": usr.acct, "fullname": usr.fullname, "created_at": usr.created_at, "updated_at": usr.updated_at}
         users_response.append(usr)
-    return JsonResponse({"users": users_response})
+    return JsonResponse({"result":SUCCESS, "message": SUCCESS, "users": users_response})
 
 def search_user(request):
+    if not check_request_method(request, 'POST'):
+        return JsonResponse({"result": FAILURE, "message": "wrong request method"}, status=405)
+
     token_verified, account_in_token = verify_token(request)
     if not token_verified:
-        raise exceptions.AuthenticationFailed("token verified failed.")
-    Session = sqlalchemy.orm.sessionmaker(bind=engine)
-    s = Session()
+        return JsonResponse({"result": FAILURE, "message": "token verification failure"}, status=403)
 
-    result = ''
-    msg = ''
-    d = json.loads(request.body)
+    try:
+        d = json.loads(request.body)
+    except ValueError as e:
+        logger.error("body:%r not a json data", request.body)
+        return JsonResponse({"result": FAILURE, "message": "wrong request body"}, status=400)
+
     if "fullname" in d:
         fullname = d["fullname"]
+        Session = sqlalchemy.orm.sessionmaker(bind=engine)
+        s = Session()
+        users = []
+        user = s.query(Users).filter(Users.fullname==fullname).all()
+        if len(user) > 0:
+            for usr in user:
+                users.append({"account":usr.acct, "fullname":usr.fullname, "created_at":usr.created_at, "updated_at":usr.updated_at})
+            msg = "{0} user(s) found.".format(len(user))
+        else:
+            msg = "no user has fullname:'{0}'.".format(fullname)
+        resp = {"result": SUCCESS, "message": msg}
+        if len(users) > 0:
+            resp["users"] = users
+        return JsonResponse(resp)
     else:
-        result = "failure"
-        msg = "fullname is not provided."
-    users = []
-    user = s.query(Users).filter(Users.fullname==fullname).all()
-    if len(user) > 0:
-        for usr in user:
-            users.append({"account":usr.acct, "fullname":usr.fullname, "created_at":usr.created_at, "updated_at":usr.updated_at})
-        result = "ok"
-        msg = "{0} user(s) found.".format(len(user))
-    else:
-        result = "ok"
-        msg = "user '{0}' not existed.".format(fullname)
-
-    resp = {"result": result, "message": msg}
-    if len(users) > 0:
-        resp["users"] = users
-
-    return JsonResponse(resp)
+        return JsonResponse({"result": FAILURE, "message": "fullname not provided."}, status=400)
 
 def user_detail(request, account):
     if not check_request_method(request, 'GET'):
-        return JsonResponse({"result": FAILURE, "msg": "wrong request method"}, status=405)
+        return JsonResponse({"result": FAILURE, "message": "wrong request method"}, status=405)
 
     token_verified, account_in_token = verify_token(request)
     if not token_verified:
-        return JsonResponse({"result": FAILURE, "msg": "token verification failure"}, status=403)
+        return JsonResponse({"result": FAILURE, "message": "token verification failure"}, status=403)
     Session = sqlalchemy.orm.sessionmaker(bind=engine)
     s = Session()
     user = s.query(Users).filter_by(acct=account).first()
@@ -189,25 +161,35 @@ def user_detail(request, account):
         result = SUCCESS
         msg = "query success."
         user_info = {"account": account, "fullname": user.fullname, "created_at": user.created_at, "updated_at": user.updated_at}
-        return JsonResponse({"result": result, "msg": msg, "user_detail": user_info})
-    return JsonResponse({"result": SUCCESS, "msg": "no such user."})
+        return JsonResponse({"result": result, "message": msg, "user_detail": user_info})
+    return JsonResponse({"result": SUCCESS, "message": "no such user."})
 
 def update_user(request):
     if not check_request_method(request, 'PUT'):
-        return JsonResponse({"result": FAILURE, "msg": "wrong request method"}, status=405)
+        return JsonResponse({"result": FAILURE, "message": "wrong request method"}, status=405)
 
-    token_verified, account_in_token = verify_token(request)
-    if not token_verified:
-        return JsonResponse({"result": FAILURE, "msg": "token verification failure"}, status=403)
+    try:
+        d = json.loads(request.body)
+    except ValueError as e:
+        logger.error("body:%r not a json data", request.body)
+        return JsonResponse({"result": FAILURE, "message": "wrong request body"}, status=400)
 
-    d = json.loads(request.body)
-    # we should check the account from jwt is correct or not
-    acct = d["account"]
+    if "account" in d:
+        acct = d["account"]
+    else:
+        return JsonResponse({"result": FAILURE, "message": "no account information."}, status=400)
+
     Session = sqlalchemy.orm.sessionmaker(bind=engine)
     s = Session()
-
     user = s.query(Users).filter_by(acct=acct).first()
     if user:
+        # the account exist, we should verify its jwt token
+        token_verified, account_in_token = verify_token(request)
+        if not token_verified:
+            return JsonResponse({"result": FAILURE, "message": "token verification failure"}, status=403)
+        if acct != account_in_token:
+            return JsonResponse({"result": FAILURE, "message": "user mismatch."}, status=400)
+
         updated = False
         if "pwd" in d:
             user.pwd = d["pwd"]
@@ -220,16 +202,51 @@ def update_user(request):
             dt = dt_tz.replace(tzinfo=None)
             user.updated_at = dt
         s.commit()
+        return JsonResponse({"result": SUCCESS, "message": "account updated."})
 
-    return JsonResponse({"result": SUCCESS, "msg": "updated successfully"})
+    else:
+        # new user
+        if "pwd" in d:
+            passwd = d["pwd"]
+            salt = bcrypt.gensalt()
+            hashed = bcrypt.hashpw(passwd.encode('utf-8'), salt)
+        else:
+            return JsonResponse({"result": FAILURE, "message": "no password information."}, status=400)
 
-def delete_user(request, account):
-    if not check_request_method(request, 'DELETE'):
-        return JsonResponse({"result": FAILURE, "msg": "wrong request method"}, status=405)
+        usr = Users()
+        usr.acct = acct
+        usr.pwd = hashed.decode('utf-8')
+        if "fullname" in d:
+            user_fullname = d["fullname"]
+        else:
+            user_fullname = acct
+        usr.fullname = user_fullname
+        dt_tz = datetime.datetime.now()
+        dt = dt_tz.replace(tzinfo=None)
+        usr.created_at = dt
+        usr.updated_at = dt
+        s.add(usr)
+        s.commit()
+        return JsonResponse({"result": SUCCESS, "message": "account created.", "token": gen_token(usr)}, status=201)
 
     token_verified, account_in_token = verify_token(request)
     if not token_verified:
-        return JsonResponse({"result": FAILURE, "msg": "token verification failure"}, status=403)
+        return JsonResponse({"result": FAILURE, "message": "token verification failure"}, status=403)
+
+    # we should check the account from jwt is correct or not
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    s = Session()
+
+
+    return JsonResponse({"result": SUCCESS, "message": "updated successfully"})
+
+def delete_user(request, account):
+    if not check_request_method(request, 'DELETE'):
+        return JsonResponse({"result": FAILURE, "message": "wrong request method"}, status=405)
+
+    token_verified, account_in_token = verify_token(request)
+    if not token_verified:
+        return JsonResponse({"result": FAILURE, "message": "token verification failure"}, status=403)
 
     logger.info("account:%r", account)
 
@@ -240,9 +257,9 @@ def delete_user(request, account):
         if user:
             s.delete(user)
             s.commit()
-            return JsonResponse({"result": SUCCESS, "msg": "deleted successfully"})
+            return JsonResponse({"result": SUCCESS, "message": "deleted successfully"})
         else:
-            return JsonResponse({"result": FAILURE, "msg": "no such user."}, status=400)
+            return JsonResponse({"result": FAILURE, "message": "no such user."}, status=400)
     else:
         #delete your own account only
-        return JsonResponse({"result": FAILURE, "msg": "user mismatch."}, status=400)
+        return JsonResponse({"result": FAILURE, "message": "user mismatch."}, status=400)
