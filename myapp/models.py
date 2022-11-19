7b0ffad1a905e580a4a71fd8ca1ae58271e5483d
chunhua chen <chunhua.kpg@gmail.com>
2022-11-19 12:51:56 +0000

    refine model

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, TIMESTAMP
-
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 
 class Users(Base):
     __tablename__ = 'Users'
@@ -14,10 +12,11 @@ class Users(Base):
     created_at = Column(TIMESTAMP)
     updated_at = Column(TIMESTAMP)
 
-
+'''
 class DjangoUsers(models.Model):
     acct = models.CharField(max_length=64, primary_key=True)
     pwd = models.CharField(max_length=128)
     fullname = models.CharField(max_length=64)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+'''
